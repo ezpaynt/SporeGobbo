7b78cc6e622e37bb8cf68e724fe4bb569a54bb5c
ezpaynt <ezpaynt@gmail.com>
2026-06-22 12:29:25 -0500

    Mark retreat portal returns as retreat

--- Assets/RunRetreatPortal.cs
+++ Assets/RunRetreatPortal.cs
@@ -67,6 +67,7 @@ public class RunRetreatPortal : MonoBehaviour, ICampInteractable
             campSceneName,
             saveRunBeforeLeaving,
             saveSlotAfterRunCommit,
+            RunReturnReason.Retreat,
             "spawn retreat portal");
     }
 
